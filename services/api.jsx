eb15d243150eaa861e3cadf8d2baf0d9034c240f
GustavoCassimiro9 <gustavocassimiro60@gmail.com>
2022-11-13 15:01:27 -0300

    Adicionando o cadastro de cadeira

--- services/api.jsx
+++ services/api.jsx
@@ -2,7 +2,7 @@ import axios from 'axios';
 import { getToken } from "./auth";
 
 const api = axios.create({
-  baseURL: "http://localhost:3000" 
+  baseURL: "http://localhost:8000" 
 });
 
 // interceptor de autenticação
