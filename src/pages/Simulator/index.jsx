d05f5ea990c2a09c43eff8609f38ca80ae33bbc7
iJacKP <jackson_d2@outlook.com>
2022-12-14 12:23:38 -0300

    bug cadastro

--- src/pages/Simulator/index.jsx
+++ src/pages/Simulator/index.jsx
@@ -2,7 +2,7 @@ import { Col, Container, Row } from "react-bootstrap"
 import Sidebar from "../../components/SideBar"
 import Button from 'react-bootstrap/Button';
 import { useState, useEffect } from "react";
-import * as AiIcons  from 'react-icons/ai';
+import * as AiIcons from 'react-icons/ai';
 import api from "../../../services/api";
 import "./index.css"
 
@@ -10,11 +10,10 @@ export function Simulator() {
 
     //Variável que guarda os dados das cadeiras
     const [rest, setRest] = useState([])
-    const [boxColor, setBoxColor] = useState(false)
 
     //Puxando as cadeiras do banco e passando os dados da resposta por meio de uma variável
-   
-    const buscarCadeiras= async () => await api.get("/cadeiras",
+
+    const buscarCadeiras = async () => await api.get("/cadeiras",
         {
         }).then(response => setRest(response.data.map(function (cadeiras) {
             const allData = [cadeiras.Name, cadeiras.Trilha, cadeiras.Horario, cadeiras.Dia, cadeiras.Professor, cadeiras.Sobre, cadeiras._id]
@@ -29,7 +28,7 @@ export function Simulator() {
 
         buscarCadeiras()
 
-    },[])
+    }, [])
 
     const atualizandoDadosDaTabela = (h, n, d) => {
 
@@ -41,7 +40,7 @@ export function Simulator() {
             let razaoHorario = Math.abs(horario[0] - horario[1])
             let mesclar = dia[0] + horario[0]
             let mesclar2 = dia[1] + horario[0]
-            let mesclar4H = dia[0] + (horario[1]-2)
+            let mesclar4H = dia[0] + (horario[1] - 2)
             let mesclandoDados = mesclar.toString()
             let mesclandoDados2 = mesclar2.toString()
             let menclandoDados3 = mesclar4H.toString()
@@ -51,25 +50,24 @@ export function Simulator() {
             if (razaoHorario >= 4) {
 
 
-                doc1.style = "background-color: #6FCAF6", doc3.style = "background-color: #6FCAF6"
+                doc1.style = "background-color: #6FCAF6 !important", doc3.style = "background-color: #6FCAF6 !important"
             } else {
 
 
-                doc1.style = "background-color: #6FCAF6", doc2.style = "background-color: #6FCAF6"
+                doc1.style = "background-color: #6FCAF6 !important", doc2.style = "background-color: #6FCAF6 !important"
             }
 
             let btn = document.getElementsByClassName("btn")
-    
+
             for (let i = 0; i <= btn.length; i++) {
 
                 btn[i].addEventListener('mouseleave', function () {
 
-                    if (razaoHorario >= 4 && !boxColor) {
-                        return doc1.style = "background-color: white", doc3.style = "background-color: white", setHorario(null), setDia(null),setBoxColor(true)
-                    } else if(razaoHorario <= 4 && !boxColor) {
-                        return doc1.style = "background-color: white", doc2.style = "background-color: white", setHorario(null), setDia(null),setBoxColor(true)
+                    if (razaoHorario >= 4) {
+                        return doc1.style = "background-color: white !important", doc3.style = "background-color: white !important", setHorario(null), setDia(null)
+                    } else if (razaoHorario <= 4) {
+                        return doc1.style = "background-color: white !important", doc2.style = "background-color: white !important", setHorario(null), setDia(null)
                     }
-
                 });
             }
 
@@ -80,48 +78,54 @@ export function Simulator() {
 
     }
 
-    const preenchendoTabela = (h, n,d) => {
+    const preenchendoTabela = (h, n, d) => {
         var horario = h
         var nome = n
         var dia = d
 
         let razaoHorario = Math.abs(horario[0] - horario[1])
-            let mesclar = dia[0] + horario[0]
-            let mesclar2 = dia[1] + horario[0]
-            let mesclar4H = dia[0] + (horario[1]-2)
-            let mesclandoDados = mesclar.toString()
-            let mesclandoDados2 = mesclar2.toString()
-            let menclandoDados3 = mesclar4H.toString()
-            console.log(boxColor)
-            let doc1 = document.getElementById(mesclandoDados)
-            let doc2 = document.getElementById(mesclandoDados2)
-            let doc3 = document.getElementById(menclandoDados3)
-            
-        setBoxColor(true)
-        if (razaoHorario >= 4 && boxColor == true) {
-            
-            return doc1.innerHTML = nome, doc3.innerHTML = nome, doc1.style = "background-color: #6ECBF5", doc3.style = "background-color: #6ECBF5"
-            
-        } else if(razaoHorario <= 4 && boxColor == true){
-            
-            return doc1.innerHTML = nome, doc2.innerHTML = nome, doc1.style = "background-color: #6ECBF5", doc2.style = "background-color: #6ECBF5"
-            
+        let mesclar = dia[0] + horario[0]
+        let mesclar2 = dia[1] + horario[0]
+        let mesclar4H = dia[0] + (horario[1] - 2)
+        let mesclandoDados = mesclar.toString()
+        let mesclandoDados2 = mesclar2.toString()
+        let menclandoDados3 = mesclar4H.toString()
+
+        let doc1 = document.getElementById(mesclandoDados)
+        let doc2 = document.getElementById(mesclandoDados2)
+        let doc3 = document.getElementById(menclandoDados3)
+
+
+        if (razaoHorario >= 4) {
+
+            return doc1.innerHTML = nome, doc3.innerHTML = nome
+
+        } else {
+
+            return doc1.innerHTML = nome, doc2.innerHTML = nome
+
         }
 
+
+    }
+    const limpandoTabela = () => {
+        const table = document.getElementsByClassName("Segunda")
+        for (let i = 0; i <= table.length; i++) {
+            table[i].innerHTML = ""
+            table[i].style = "background-color: white"
+        }
 
     }
 
     const body = <>
         <Container fluid>
 
-             
-                <div className='d-flex justify-content-md-end mb-4' style={{width:'100%'}}>
-                 <Button onClick={() => window.print()} style={{ display:"flex", alignItems:'flex-end', justifyContent:'flex-end', justifyItems:'flex-end',flexDirection:"row" ,backgroundColor:'#2A2356', borderColor:'#2A2356'}}>  <AiIcons.AiOutlineFilePdf size={35} />  </Button>
-                </div>
 
-            <Row>
 
-                <Col style={{width:'100%'}}>
+
+            <Row >
+
+                <Col lg={8}>
                     <table>
                         <thead>
                             <tr>
@@ -194,25 +198,39 @@ export function Simulator() {
                     </table>
                 </Col>
 
-                <Col>
-                    <ul style={{display:"flex",flexWrap:'wrap', width:'100%'}}>
-                        {
+                <Col lg={4}>
+                    <div style={{ height: "500px", overflowY: "hidden", display:'flex', justifyContent:'center', }}>
+                        <ul style={{ listStyle: 'none', flexWrap: 'wrap', width: '100%' }}>
+                            {
 
-                        rest && rest.map( (item, index) => {
-                                console.log("aaa",item)
-                                return (
-                                    <li  ><Button style={{backgroundColor:'#2A2356'}} onClick={() => preenchendoTabela(item[2], item[0], item[3])} onMouseOver={() => atualizandoDadosDaTabela(item[2], item[0], item[3])} className="p-3 m-2" > {item[0]}</Button></li>
-                                )
-                            })
+                                rest && rest.map((item, index) => {
+                                    console.log("aaa", item)
+                                    return (
+                                        <li style={{ paddingBottom: "5px", width: "initial" }}><Button style={{ backgroundColor: '#2A2356', width: "250px" }} onClick={() => preenchendoTabela(item[2], item[0], item[3])} onMouseOver={() => atualizandoDadosDaTabela(item[2], item[0], item[3])} className="btn p-3 m-2" > {item[0]}</Button></li>
+                                    )
+                                })
 
-                        }
-                    </ul>
+                            }
+                        </ul>
+                    </div>
                 </Col>
 
 
             </Row>
 
 
+
+            <Row>
+                <Col md={8}>
+
+                    <div className='d-flex  mb-4' style={{ width: '100%', marginTop: "30px"}}>
+                        <Button onClick={limpandoTabela} style={{ width: "40%" , backgroundColor: '#2A2359', marginLeft: "100px"}}>Limpar</Button>
+                        <Button onClick={() => window.print()} style={{ width: "40%", marginLeft: "10px", textAlign: "center", flexDirection: "row", backgroundColor: '#2A2356', borderColor: '#2A2356' }}>Salvar como   <AiIcons.AiOutlineFilePdf size={35} />  </Button>
+                    </div>
+                <Col md={4}></Col>
+                </Col>
+
+            </Row>
 
         </Container>
     </>
